86e09126e9b714be9bfe58f4424aed5786c884d7
haoran1109 <940279663@qq.com>
2020-04-21 22:05:41 +0800

    product/**  api都无需鉴权

---
+++ shop-provider/shop-provider-mdc/src/main/java/com/shop/config/ResourceServerConfig.java
@@ -0,0 +1,32 @@
+
+
+package com.shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The class Resource server config.
+ *
+ *
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+				.headers().frameOptions().disable()
+				.and()
+				.csrf().disable()
+				.exceptionHandling()
+				.authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+				.and()
+				.authorizeRequests().antMatchers("/pay/alipayCallback", "/druid/**", "/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/product/**","/api/applications","/product/**").permitAll()
+				.anyRequest().authenticated();
+	}
+}
